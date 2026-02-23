e435f3d27d7ac0d8e8ab9180ce4c961906aff8a9
gohsa5458 <gohsa5458@gmail.com>
2026-02-24 01:31:22 +0800

    Extract welcome message

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -9,8 +9,9 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Task manager (using streams)");
-        Datamanager dataManager = new Datamanager("./data/data.txt");
+        printWelcomeMessage();
+        Datamanager dataManager = new Datamanager("./data/data.txt"); // relative path
+        //" C:\\Users\\gohsa\\IdeaProjects\\Tasks\\data" <<< absolute path
         ArrayList<Task> tasksData = dataManager.loadData();
 
         System.out.println("Printing all data ...");
@@ -21,6 +22,10 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+    }
+
+    private static void printWelcomeMessage() {
+        System.out.println("Welcome to Task manager (using streams)");
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
